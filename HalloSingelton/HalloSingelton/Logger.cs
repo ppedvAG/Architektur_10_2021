a2aa037966c0e0a13f74a9aa0777e67559b02a76
André <AndreR@outlook.de>
2021-10-04 12:12:56 +0200

    Threadafe Singelton

---
+++ HalloSingelton/HalloSingelton/Logger.cs
@@ -0,0 +1,34 @@
+﻿namespace HalloSingelton
+{
+    class Logger
+    {
+        private static Logger _instance;
+        private static object _instanceLock = new object();
+
+        public static Logger Instance
+        {
+            get
+            {
+                lock (_instanceLock)
+                {
+                    if (_instance == null)
+                        _instance = new Logger();
+                }
+
+                return _instance;
+            }
+        }
+
+        static int instCounter = 0;
+        private Logger()
+        {
+            Console.WriteLine("CTOR Singelton");
+            instCounter++;
+        }
+
+        public void Log(string msg)
+        {
+            Console.WriteLine($"({instCounter}) [{DateTime.Now}] {msg}");
+        }
+    }
+}
---
+++ HalloSingelton/HalloSingelton/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using HalloSingelton;
+
+//Console.WriteLine("Hello, World!");
+
+//new Logger().Log("Hallo Logger");
+
+for (int i = 0; i < 100; i++)
+{
+    Task.Run(() => Logger.Instance.Log("Hallo Logger"));
+}
+
+
+//Console.WriteLine("Ende");
+Console.ReadLine();
