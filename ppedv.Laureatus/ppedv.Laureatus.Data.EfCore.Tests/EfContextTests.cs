93987800227237d6e2fad1aff994c14b3c5aed2f
André <ruland@outlook.de>
2021-10-05 14:27:03 +0200

    Person speichern und lesen mit AutoFixture und FluentAssertions

--- ppedv.Laureatus/ppedv.Laureatus.Data.EfCore.Tests/EfContextTests.cs
+++ ppedv.Laureatus/ppedv.Laureatus.Data.EfCore.Tests/EfContextTests.cs
@@ -1,5 +1,11 @@
+using AutoFixture;
+using AutoFixture.Kernel;
+using FluentAssertions;
 using ppedv.Laureatus.Model;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using Xunit;
 
 namespace ppedv.Laureatus.Data.EfCore.Tests
@@ -31,9 +37,12 @@ namespace ppedv.Laureatus.Data.EfCore.Tests
             {
                 //check created
                 var loaded = con.Persons.Find(p.Id);
-                Assert.NotEqual(DateTime.MinValue, loaded.Created);
-                Assert.NotEqual(DateTime.MinValue, loaded.Modified);
-                Assert.Equal(loaded.Created, loaded.Modified);
+                //Assert.NotEqual(DateTime.MinValue, loaded.Created);
+                //Assert.NotEqual(DateTime.MinValue, loaded.Modified);
+                //Assert.Equal(loaded.Created, loaded.Modified);
+                loaded.Created.Should().NotBe(DateTime.MinValue);
+                loaded.Modified.Should().NotBe(DateTime.MinValue);
+                loaded.Modified.Should().Be(loaded.Modified);
 
                 loaded.Name = "Willi";
                 con.SaveChanges();
@@ -43,7 +52,51 @@ namespace ppedv.Laureatus.Data.EfCore.Tests
             {
                 //check modified
                 var loaded = con.Persons.Find(p.Id);
-                Assert.NotEqual(loaded.Created, loaded.Modified);
+                //Assert.NotEqual(loaded.Created, loaded.Modified);
+                loaded.Modified.Should().NotBe(loaded.Created);
+                loaded.Modified.Should().BeCloseTo(DateTime.Now, TimeSpan.FromMilliseconds(1000));
+            }
+        }
+
+        [Fact]
+        public void Can_create_and_read_Person()
+        {
+            var fix = new Fixture();
+            fix.Behaviors.Add(new OmitOnRecursionBehavior());
+            fix.Customizations.Add(new PropertyNameOmitter(nameof(Entity.Id)));
+            var p = fix.Create<Person>();
+
+            //CREATE
+            using (var con = new EfContext())
+            {
+                con.Add(p);
+                con.SaveChanges();
+            }
+
+            using (var con = new EfContext())
+            {
+                //READ
+                var loaded = con.Persons?.Find(p.Id);
+                loaded.Should().BeEquivalentTo(p, c => c.IgnoringCyclicReferences());
+            }
+        }
+
+        internal class PropertyNameOmitter : ISpecimenBuilder
+        {
+            private readonly IEnumerable<string> names;
+
+            internal PropertyNameOmitter(params string[] names)
+            {
+                this.names = names;
+            }
+
+            public object Create(object request, ISpecimenContext context)
+            {
+                var propInfo = request as PropertyInfo;
+                if (propInfo != null && names.Contains(propInfo.Name))
+                    return new OmitSpecimen();
+
+                return new NoSpecimen();
             }
         }
 
