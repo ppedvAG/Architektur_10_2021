f4c4910d0f7cae3b510a66e5afe2b38a44b6427f
André <ruland@outlook.de>
2021-10-05 11:25:35 +0200

    EfCore ist fertig konfiguriert

---
+++ ppedv.Laureatus/ppedv.Laureatus.Data.EfCore/EfContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ppedv.Laureatus.Model;
+
+namespace ppedv.Laureatus.Data.EfCore
+{
+    public class EfContext : DbContext
+    {
+        private readonly string conString;
+
+        public DbSet<Person>? Persons { get; set; }
+        public DbSet<Price>? Prices { get; set; }
+        public DbSet<Laureate>? Laureates { get; set; }
+        public DbSet<Category>? Categories { get; set; }
+
+        public EfContext(string conString = "Server=(localdb)\\mssqllocaldb;Database=Laureatus;Trusted_Connection=true")
+        {
+            this.conString = conString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(conString).UseLazyLoadingProxies();
+        }
+    }
+}
