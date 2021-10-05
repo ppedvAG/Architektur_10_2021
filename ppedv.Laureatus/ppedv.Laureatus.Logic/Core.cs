04fb2e9d559f5c908760e4606f23e9497b002a3c
André <ruland@outlook.de>
2021-10-05 15:18:57 +0200

    Geht, aber schlecht weil wir direkt auf Klassen coden.

---
+++ ppedv.Laureatus/ppedv.Laureatus.Logic/Core.cs
@@ -0,0 +1,15 @@
+﻿using ppedv.Laureatus.Data.EfCore;
+using ppedv.Laureatus.Model;
+
+namespace ppedv.Laureatus.Logic
+{
+    public class Core
+    {
+        public IEnumerable<Person> GetWinnersOfYear(int year)
+        {
+            var con = new EfContext();
+            return con.Persons.Where(x => x.Laureates.Any(y => y.Price.Year == year))
+                              .OrderBy(x => x.Name);
+        }
+    }
+}
---
+++ ppedv.Laureatus/ppedv.Laureatus.UI.DevConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ppedv.Laureatus.Logic;
+
+Console.WriteLine("Hello, World!");
+
+var core = new Core();
+foreach (var person in core.GetWinnersOfYear(2222))
+{
+    Console.WriteLine($"{person.Name}");
+}
+
+Console.WriteLine("Ende");
+Console.ReadLine();
