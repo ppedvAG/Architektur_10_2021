3a9fa7204211261f4b2344048b5ed1f7694e5427
André <AndreR@outlook.de>
2021-10-06 16:53:55 +0200

    Basic MVVM in WPF

---
+++ ppedv.Laureatus/ppedv.Laureatus.UI.Wpf/ViewModels/PersonViewModel.cs
@@ -0,0 +1,73 @@
+﻿using ppedv.Laureatus.Logic;
+using ppedv.Laureatus.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+
+namespace ppedv.Laureatus.UI.Wpf.ViewModels
+{
+
+    public class PersonViewModel : ViewModelBase
+    {
+        public Person SelectedPerson
+        {
+            get => selectedPerson;
+            set
+            {
+                selectedPerson = value;
+                OnPropertyChanged(""); //alle
+                //OnPropertyChanged(nameof(Alter));
+                //OnMyPropertyChanged();
+            }
+        }
+
+        public ICommand SaveCommand { get; set; }
+        public ICommand NewCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
+
+
+        public ObservableCollection<Person> Personen { get; set; }
+
+
+        public int Alter
+        {
+            get
+            {
+                return random.Next();
+            }
+        }
+
+
+        Random random = new Random();
+
+        Core core = new Core(new Data.EfCore.EfRepository());
+        private Person selectedPerson;
+
+        public PersonViewModel()
+        {
+            Personen = new ObservableCollection<Person>(core.Repository.Query<Person>().ToList());
+            SaveCommand = new RelayCommand(x => core.Repository.Save());
+
+            NewCommand = new RelayCommand(UserWantsToAddNewPerson);
+            DeleteCommand = new RelayCommand(UserWantsToDeleteSelectedPerson);
+        }
+
+        private void UserWantsToDeleteSelectedPerson(object obj)
+        {
+            if (selectedPerson != null)
+            {
+                core.Repository.Delete(SelectedPerson);
+                Personen.Remove(selectedPerson);
+            }
+        }
+
+        private void UserWantsToAddNewPerson(object obj)
+        {
+            var p = new Person() { Name = "WPF NEU" };
+            core.Repository.Add(p);
+            Personen.Add(p);
+        }
+    }
+}
---
+++ ppedv.Laureatus/ppedv.Laureatus.UI.Wpf/ViewModels/ViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ppedv.Laureatus.UI.Wpf.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+
+        protected void OnMyPropertyChanged([CallerMemberName] string propName = "") => OnPropertyChanged(propName);
+}
+}
